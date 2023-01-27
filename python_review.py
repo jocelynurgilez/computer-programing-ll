3ac45a1d9527b067c7846b26bb21d95af9dcf285
Jocelyn Urgilez <jocelynurgilez@Jocelyns-MacBook-Air.local>
2023-01-27 10:20:23 -0600

    initial commit

---
+++ python_review.py
@@ -0,0 +1,17 @@
+print("hello")
+
+number_1=int( input("Please enter a number: "))
+number_2=int( input("Please enter a second number: "))
+
+total = number_1 + number_2
+print(total)
+
+if total > 10:
+	print("boooo")
+elif total <= 9:
+	print("sure")
+else: # must be the value of 10
+	print("your number is 10!")
+
+for i in range:
+	
